76f8a1e1c0ac928275fa268c6603a4fe2b9be530
Boris Philipp <engs-borisluz@camporeal.edu.br>
2025-03-24 14:35:50 -0300

    teste com 2 colunas

---
+++ Desenvolvimento-de-Sistemas-Web-Mobile-III/1-bimestre/primeiro-prototipo-projeto/public/pagina-produto.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style_pagina-produto.css">
+
+    <title>DumpStore - Produto</title>
+</head>
+<body>
+    <h1>Pagina de produto</h1>
+    <div class="row">
+        <div class="column"><a href="#">teste</a></div>
+        <div class="column"><a href="#">teste</a></div>
+    </div>
+</body>
+</html>
