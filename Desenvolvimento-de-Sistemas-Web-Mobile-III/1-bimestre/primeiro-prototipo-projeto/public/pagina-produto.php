ea4266e955f83dac8065f1eb3b126bba1c59cf1b
Boris Philipp <engs-borisluz@camporeal.edu.br>
2025-03-24 15:57:15 -0300

    reformulando o codigo

--- Desenvolvimento-de-Sistemas-Web-Mobile-III/1-bimestre/primeiro-prototipo-projeto/public/pagina-produto.php
+++ Desenvolvimento-de-Sistemas-Web-Mobile-III/1-bimestre/primeiro-prototipo-projeto/public/pagina-produto.php
@@ -12,9 +12,6 @@
         <h1>Pagina de produto</h1>
     </div>
 
-    <div class="row">
-        <div class="column"><a href="#">teste</a></div>
-        <div class="column"><a href="#">teste</a></div>
-    </div>
+
 </body>
 </html>
