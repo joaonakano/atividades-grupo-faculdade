056c176f2b5bdbfe97201fddd63713cb30959742
Boris Philipp <engs-borisluz@camporeal.edu.br>
2025-03-24 18:49:57 -0300

    estilização e adicao de contexto

--- Desenvolvimento-de-Sistemas-Web-Mobile-III/1-bimestre/primeiro-prototipo-projeto/public/pagina-produto.php
+++ Desenvolvimento-de-Sistemas-Web-Mobile-III/1-bimestre/primeiro-prototipo-projeto/public/pagina-produto.php
@@ -4,12 +4,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style_pagina-produto.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Rubik+Glitch&display=swap" rel="stylesheet">
 
     <title>DumpStore - Produto</title>
 </head>
 <body>
-    <div class="container-title">
-        <h1>Pagina de produto</h1>
+<!--     <div class="container-title">
+        <h1>Pagina de produto <br> Nome do produto provavelmente</h1>
+    </div> -->
+
+    <div class="container-produto">
+        <div class="item">
+            <img src="images/crash_jogo_ps1.jpg" alt="Capa do jogo Crash Bandicoot de PlayStation 1.">
+
+            <article>
+                <h1>CRASH BANDICOOT</h1>
+                <h2>SINOPSE</h2>
+                <p>Após dar errado transformar <b>Crash</b> em um super soldado,
+                <b>Neo Cortex</b> sai correndo atrás de Crash, mas o <b>bandicoot</b> foge e mantem sua namorada cativa.
+                Crash deve correr várias fases, derrotar vilões, pegar jóias para salvar sua <b>namorada</b>. </p>
+
+                <h3>Preço - R$ 5</h3>
+
+
+            </article>
+            
+            <img src="images/crash_ps1.gif" alt="Gif do Crash correndo de uma pedra rolante.">
+        </div>
     </div>
 
 
