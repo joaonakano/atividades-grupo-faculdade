6a8dad44191f924f2db8a68f24485408761dbee4
Boris Philipp <engs-borisluz@camporeal.edu.br>
2025-03-25 09:30:43 -0300

    remocao de linhas de codigo inuteis

--- Desenvolvimento-de-Sistemas-Web-Mobile-III/1-bimestre/primeiro-prototipo-projeto/public/pagina-produto.php
+++ Desenvolvimento-de-Sistemas-Web-Mobile-III/1-bimestre/primeiro-prototipo-projeto/public/pagina-produto.php
@@ -4,20 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style_pagina-produto.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-
     <title>DumpStore - Produto</title>
 </head>
 <body>
     
-<!--     <div class="container-title">
-        <h1>Pagina de produto <br> Nome do produto provavelmente</h1>
-    </div> -->
-
     <div class="container-produto">
         <div class="item">
-            <img src="images/crash_jogo_ps1.jpg" alt="Capa do jogo Crash Bandicoot de PlayStation 1.">
+            <img src="images/crash_jogo_ps1.jpg" alt="Capa do jogo Crash Bandicoot de PlayStation 1." title="Capa do jogo Crash Bandicoot de PlayStation 1.">
 
             <article>
                 <h1>CRASH BANDICOOT</h1>
@@ -29,17 +22,16 @@
                 <h3>Preço - R$ 5</h3><br>
             </article>
             
-            <img src="images/crash_ps1.gif" alt="Gif do Crash correndo de uma pedra rolante."><br>
+            <img src="images/crash_ps1.gif" alt="Gif do Crash correndo de uma pedra rolante." title="Gif de um trecho do jogo."><br>
 
             <button><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">-> ALUGAR <-</a></button><br>
 
         </div>
 
-        <img src="images/ps1.gif" alt="Gif de um PlayStation 1 girando.">
+        <img src="images/ps1.gif" alt="Gif de um PlayStation 1 girando." title="Gif de um PlayStation 1 girando.">
 
-        <footer>Direitos Autorais - 1998</footer>
+        <footer><strong>© 1994 - DumpStore</strong></footer>
     </div>
-
 
 </body>
 </html>
