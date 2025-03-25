f72267d5431696d0e2b53abcdba4e9b61c5ffc6a
Emanuelprime <emanuelll3000@gmail.com>
2025-03-25 15:23:01 -0300

    mudança de estilo

--- Desenvolvimento-de-Sistemas-Web-Mobile-III/1-bimestre/primeiro-prototipo-projeto/public/index.php
+++ Desenvolvimento-de-Sistemas-Web-Mobile-III/1-bimestre/primeiro-prototipo-projeto/public/index.php
@@ -1,3 +1,4 @@
+
 <?php
 require __DIR__ . "/../src/bootstrap.php";
 require_login();
@@ -5,6 +6,21 @@ require_login();
 
 <?php view('header', ['title' => 'Dashboard']) ?>
 
-<p>Bem-vindo, <?= current_user() ?> <a href="logout.php">Logout</a></p>
+<p>Bem-vindo, ao nosso Catalogo de Jogos <br><?= current_user() ?> <br><a href="logout.php">Logout</a></p>
+
+<main>
+    <h1>Catálogo de Jogos</h1>
+
+    <div class="game-list">
+        <div class="game-item">
+            <img src="../images/crash_jogo_ps1.jpg" alt="Crash Bandicoot">
+            <h2>Crash Bandicoot</h2>
+            <p>Preço: R$ 5</p>
+            <button>Editar</button>
+            <button>Excluir</button>
+        </div>
+        <!-- Adicione mais jogos aqui -->
+    </div>
+</main>
 
 <?php view('footer') ?>
